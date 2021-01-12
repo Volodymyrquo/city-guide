d977b25f29351e0a5ad17f4ee0ea846f234974e8
Volodymyr <volodymyrquo@gmail.com>
2021-01-12 09:58:24 +0200

    added Detail component

--- src/components/pages/Details.jsx
+++ src/components/pages/Details.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { InfoConsumer } from '../../context';
 
 const Details = () => {
   return (
-    <div>
-      <h1>Details page ...</h1>
-    </div>
+    <InfoConsumer>
+      {(data) => {
+        const {
+          id,
+          headerTitle,
+          headerSubTitle,
+          headerText,
+          title,
+          description,
+          img,
+          maps,
+        } = data.detailInfo;
+        return (
+          <>
+            <div className='container-fluid align-items-center'>
+              <h1 className='display-1 font-weight-bold'>{headerTitle}</h1>
+              <h4 className='display-5'>{headerSubTitle}</h4>
+              <p>{headerText}</p>
+            </div>
+          </>
+        );
+      }}
+    </InfoConsumer>
   );
 };
 
---
+++ src/components/pages/Home/testDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const testDetails = ({ text }) => {
+  return (
+    <div>
+      <h1>Hello from details {text} </h1>
+    </div>
+  );
+};
+
+export default testDetails;
