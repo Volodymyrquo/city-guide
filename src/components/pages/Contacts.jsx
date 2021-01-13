fd348e539956c1de8937e4a6c493f450d8e8470b
Volodymyr <volodymyrquo@gmail.com>
2021-01-13 16:32:36 +0200

    added Contacts page

--- src/components/pages/Contacts.jsx
+++ src/components/pages/Contacts.jsx
@@ -1,10 +1,71 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Contacts = () => {
   return (
-    <div>
-      <h1>Contacts page ...</h1>
-    </div>
+    <section className='my-5 py-5'>
+      <div className='container'>
+        <div className='well well-sm'>
+          <h3>
+            <strong>Our location</strong>
+          </h3>
+        </div>
+
+        <div className='row'>
+          <div className='col-md-7'>
+            <iframe
+              src='https://www.google.com/maps/d/u/0/embed?mid=1sr9HCabDm2iKgt2tEEBk3GfYjQqczKaY'
+              title='map'
+              style={{
+                border: '0',
+                width: '100%',
+                height: '315px',
+                frameborder: '0',
+              }}
+              allowFullScreen></iframe>
+          </div>
+          <div className='col-md-5'>
+            <h4>
+              <strong>Contact Us</strong>
+            </h4>
+            <form action=''>
+              <div className='form-group'>
+                <input
+                  type='text'
+                  className='form-control'
+                  placeholder='Name'
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  type='email'
+                  className='form-control'
+                  placeholder='Email'
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  type='tel'
+                  className='form-control'
+                  placeholder='Phone'
+                />
+              </div>
+              <textarea
+                className='form-control'
+                cols='30'
+                rows='3'
+                placeholder='Message'
+              />
+              <Link className='btn btn-outline-primary text-uppercase mt-1'>
+                <i className='fa fas-paper-plane-o' aria-hidden='true' />
+                <i className='fab fa-telegram-plane' />
+                &nbsp; send
+              </Link>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/pages/News/NewsCard.jsx
+++ src/components/pages/News/NewsCard.jsx
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 const NewsCard = ({ id, newsTitle, newsText }) => {
   return (
     <div>
-      <div className='container mt-2 mb-3 p5'>
+      <div className='card container mt-2 mb-3 p-5'>
         <div className='card-body'>
           <h5 key={id} className='card-title'>
             {newsTitle}
           </h5>
           <p className='card-text'>{newsText}</p>
-          <Link className='card-link'>Read more{'>>'}</Link>
+          <Link to='/' className='card-link'>
+            Read more{'>>'}
+          </Link>
         </div>
       </div>
     </div>
